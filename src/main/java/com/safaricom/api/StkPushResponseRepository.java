942917bd790dbe69c6f0ffa4e9e24b81aa4ecbe8
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-04 20:59:27 +0300

    safaricom daraja apis, stk push,callback metadata

---
+++ src/main/java/com/safaricom/api/StkPushResponseRepository.java
@@ -0,0 +1,6 @@
+package com.safaricom.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StkPushResponseRepository extends JpaRepository<StkPushResponse, Long> {
+}
