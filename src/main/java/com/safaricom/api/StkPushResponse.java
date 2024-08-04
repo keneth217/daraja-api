7aaaa6f901a4f338f399dcf9e9b294b727bde463
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-04 20:59:15 +0300

    safaricom daraja apis, stk push,callback metadata

---
+++ src/main/java/com/safaricom/api/StkPushResponse.java
@@ -0,0 +1,20 @@
+package com.safaricom.api;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "stk_push_responses")
+public class StkPushResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String phoneNumber;
+    private double amount;
+
+    @Column(length = 2000) // or @Lob for TEXT type
+    private String response;
+    private String timestamp;
+}
