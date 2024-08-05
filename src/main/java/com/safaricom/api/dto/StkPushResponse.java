fbae387e13015a99554deef2c234f47ae7db077d
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-05 17:41:06 +0300

    call back data,persist to database

--- src/main/java/com/safaricom/api/dto/StkPushResponse.java
+++ src/main/java/com/safaricom/api/dto/StkPushResponse.java
@@ -1,19 +1,14 @@
 package com.safaricom.api.dto;
 
-import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
 @Data
-@Table(name = "stk_push_responses")
 public class StkPushResponse {
+    private String id;
+    private String merchantRequestId;
+    private String checkoutRequestId;
+    private int responseCode;
+    private String responseDescription;
+    private String customerMessage;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String phoneNumber;
-    private double amount;
-    @Column(length = 2000) // or @Lob for TEXT type
-    private String response;
-    private String timestamp;
 }
